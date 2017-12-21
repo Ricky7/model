1cdfb9a6caaa0f14973ce1e8bd98c9e62b413c29
Ricky7 <black.id64@gmail.com>
2017-12-21 15:53:34 +0700

    tambah log transaksi model

---
+++ application/models/Logtransaksi_model.php
@@ -0,0 +1,145 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Logtransaksi_model extends CI_Model{
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->database();
+  }
+
+  // jenis log
+  // kode log PLN postpaid = 1
+  // kode log PLN prepaid = 2
+  // kode log PLN nontaglis  = 3
+  // kode log PDAM = 4
+  // kode log BPJS = 5
+
+  // show log by jenis log
+  public function getLogByJenis($jenis_log)
+  {
+      $this->db->select('*');
+      $this->db->from('inm_log_transaksi a');
+      $this->db->join('inm_jenis_log b', 'a.jenis_log = b.kode_log');
+      $this->db->where('a.jenis_log', $jenis_log);
+      $query = $this->db->get();
+      if($query->num_rows() != 0)
+      {
+          return $query->result_array();
+      }
+      else
+      {
+          return false;
+      }
+  }
+
+  // show log by User
+  public function getLogByUser($id_user)
+  {
+      $this->db->select('*');
+      $this->db->from('inm_log_transaksi a');
+      $this->db->join('inm_jenis_log b', 'a.jenis_log = b.kode_log');
+      $this->db->where('a.id_user', $id_user);
+      $query = $this->db->get();
+      if($query->num_rows() != 0)
+      {
+          return $query->result_array();
+      }
+      else
+      {
+          return false;
+      }
+  }
+
+  // show log by Jenis Log & User
+  public function getLogByUserAndJenis($id_user, $jenis_log)
+  {
+      $this->db->select('*');
+      $this->db->from('inm_log_transaksi a');
+      $this->db->join('inm_jenis_log b', 'a.jenis_log = b.kode_log');
+      $this->db->where('a.id_user', $id_user);
+      $this->db->where('a.jenis_log', $jenis_log);
+      $query = $this->db->get();
+      if($query->num_rows() != 0)
+      {
+          return $query->result_array();
+      }
+      else
+      {
+          return false;
+      }
+  }
+
+  // show log by User & Date
+  public function getLogByUserAndDate($id_user, $tanggal)
+  {
+      $this->db->select('*');
+      $this->db->from('inm_log_transaksi a');
+      $this->db->join('inm_jenis_log b', 'a.jenis_log = b.kode_log');
+      $this->db->where('a.id_user', $id_user);
+      $this->db->where('a.tgl_create', $tanggal);
+      $query = $this->db->get();
+      if($query->num_rows() != 0)
+      {
+          return $query->result_array();
+      }
+      else
+      {
+          return false;
+      }
+  }
+
+  // show log by User & Start Date - End Date
+  public function getLogByUserAndIntervalDate($id_user, $start_date, $end_date)
+  {
+      $this->db->select('*');
+      $this->db->from('inm_log_transaksi a');
+      $this->db->join('inm_jenis_log b', 'a.jenis_log = b.kode_log');
+      $this->db->where('a.id_user', $id_user);
+      $this->db->where('a.tgl_create >=', $start_date);
+      $this->db->where('a.tgl_create <=', $end_date);
+      $query = $this->db->get();
+      if($query->num_rows() != 0)
+      {
+          return $query->result_array();
+      }
+      else
+      {
+          return false;
+      }
+  }
+
+  // show log by Start Date - End Date
+  public function getLogByIntervalDate($start_date, $end_date)
+  {
+      $this->db->select('*');
+      $this->db->from('inm_log_transaksi a');
+      $this->db->join('inm_jenis_log b', 'a.jenis_log = b.kode_log');
+      $this->db->where('a.tgl_create >=', $start_date);
+      $this->db->where('a.tgl_create <=', $end_date);
+      $query = $this->db->get();
+      if($query->num_rows() != 0)
+      {
+          return $query->result_array();
+      }
+      else
+      {
+          return false;
+      }
+  }
+
+  // insert log by Jenis log
+  // isi dalam array $data
+  // jenis_log  = kode_log (ada diatas)
+  // id_user = id user/loket 
+  // response_message
+  // response_json
+  // tgl_create = now
+  public function addLog($data)
+  {
+      $this->db->insert('inm_log_transaksi', $data);
+      return true;
+  }
+
+}
